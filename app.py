045647af71f5cbef31b389c1b1b01ded402ed7ca
Takako Akagi <takakoakagi0426@gmail.com>
2020-08-06 15:29:06 +0900

    python

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route("/")
+def helloworld():
+    return "Hello World!"
+
+@app.route("/name")
+def greet(name):
+        return name +"さん、はろー！"
+
+@app.route("/template")
+def template():
+    py_name="takako"
+    return render_template("index.html",name=py_name)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
